a100f6fcbdcfeadf494b2cd343671dd130c0c5b7
Travis CI User <travis@example.org>
2017-07-11 05:16:17 +0000

    Fixed golang missing dependencies

--- travis-test.sh
+++ travis-test.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 export PATH=/sbin/:$PATH
 sudo chown travis:travis /
+./get-deps.sh
 cd src/
 vido --pass-env GOPATH GOROOT GOTOOLDIR GO15VENDOREXPERIMENT --kernel=tests/linux-uml -- go test
 sudo chown root:root /
