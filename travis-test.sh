47747f45a73e84620f80eb0f75e75bf8504cbc85
Travis CI User <travis@example.org>
2017-07-11 05:10:37 +0000

    Fixed wrong uml linux name

--- travis-test.sh
+++ travis-test.sh
@@ -2,5 +2,5 @@
 export PATH=/sbin/:$PATH
 sudo chown travis:travis /
 cd src/
-vido --pass-env GOPATH GOROOT GOTOOLDIR GO15VENDOREXPERIMENT --kernel=tests/linux -- go test
+vido --pass-env GOPATH GOROOT GOTOOLDIR GO15VENDOREXPERIMENT --kernel=tests/linux-uml -- go test
 sudo chown root:root /
