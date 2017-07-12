fe4f31615bdf4d37ff52d4ce4bf3a687910cc103
Darren Wu <darren.wu@canonical.com>
2017-07-12 17:20:11 +0800

    Fixed vido test out of memory issue

--- travis-test.sh
+++ travis-test.sh
@@ -3,5 +3,5 @@ export PATH=/sbin/:$PATH
 sudo chown travis:travis /
 ./get-deps.sh
 cd src/
-vido --pass-env GOPATH GOROOT GOTOOLDIR GO15VENDOREXPERIMENT --kernel=tests/linux-uml -- go test
+vido --mem=512M --pass-env GOPATH GOROOT GOTOOLDIR --kernel=tests/linux-uml -- go test
 sudo chown root:root /
