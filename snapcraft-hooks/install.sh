0423a266763dbc7cd39e1532dae32a7262efe050
Darren Wu <darren.wu@canonical.com>
2017-09-19 11:08:02 +0800

    Add snapcraft hooks for building gadget snap

---
+++ snapcraft-hooks/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is the hook for snapcraft install of recovery part
+
+mkdir -p $SNAPCRAFT_PART_INSTALL/recovery-assets
+cp -r recovery-includes/recovery $SNAPCRAFT_PART_INSTALL/recovery-assets/
+cp -r initrd-hooks $SNAPCRAFT_PART_INSTALL/recovery-assets/
+cp ../../recovery-configs/src/config.yaml $SNAPCRAFT_PART_INSTALL/recovery-assets/recovery/
+cp -r writable-includes $SNAPCRAFT_PART_INSTALL/
+cp -r ubuntu-image-hooks $SNAPCRAFT_PART_INSTALL/
+mkenvimage -r -s 131072 -o $SNAPCRAFT_PART_INSTALL/uboot.env recovery-includes/uboot.env.in
