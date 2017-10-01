1b08eec42bc6a00b46e53632aead2f83adc9d29a
Darren Wu <darren.wu@canonical.com>
2017-10-01 23:34:01 +0800

    Modify the build.sh hook for amd64, armhr, arm64 support

--- snapcraft-hooks/build.sh
+++ snapcraft-hooks/build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 # This is the hook for snapcraft build of recovery part
 
-GOPATH=$SNAPCRAFT_PART_INSTALL/../go GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go run build.go build
+arch=$1
+
+if [ $arch == "amd64" ];then
+    GOPATH=$SNAPCRAFT_PART_INSTALL/../go go run build.go build
+elif [ $arch == "armhf" ];then
+    GOPATH=$SNAPCRAFT_PART_INSTALL/../go GOARCH=arm GOARM=7 CGO_ENABLED=1 CC=arm-linux-gnueabihf-gcc go run build.go build
+elif [ $arch == "arm64" ];then
+    GOPATH=$SNAPCRAFT_PART_INSTALL/../go GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc go build -o local-includes/recovery/bin/recovery.bin ./src/
+else
+    echo "unknown arch"
+    return -1
+fi
