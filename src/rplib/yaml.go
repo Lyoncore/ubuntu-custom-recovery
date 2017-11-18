98fe4cb34b31a8dcd6ef68d7d3cfe5e5bb440393
Darren Wu <darren.wu@canonical.com>
2017-11-19 00:59:18 +0800

    Fixed build fails

--- src/rplib/yaml.go
+++ src/rplib/yaml.go
@@ -6,8 +6,6 @@ import (
 	"io/ioutil"
 	"log"
 
-	"github.com/snapcore/snapd/logger"
-
 	"gopkg.in/yaml.v2"
 )
 
@@ -44,84 +42,84 @@ func (config *ConfigRecovery) checkConfigs() (err error) {
 
 	if config.Project == "" {
 		err = errors.New("'project' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Snaps.Kernel == "" {
 		err = errors.New("'snaps -> kernel' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Snaps.Os == "" {
 		err = errors.New("'snaps -> os' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Snaps.Gadget == "" {
 		err = errors.New("'snaps -> gadget' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.Arch == "" {
 		err = errors.New("'configs -> arch' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	} else if config.Configs.Arch != "amd64" && config.Configs.Arch != "arm" && config.Configs.Arch != "arm64" && config.Configs.Arch != "armhf" {
 		err = errors.New("'recovery -> arch' only accept \"amd64\" or \"arm\" or \"arm64\" or \"amdhf\"")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.BaseImage == "" {
 		err = errors.New("'configs -> baseimage' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.Release == "" {
 		err = errors.New("'configs -> release' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.PartitionType == "" {
 		err = errors.New("'recovery -> PartitionType' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	} else if config.Configs.PartitionType != "gpt" && config.Configs.PartitionType != "mbr" {
 		err = errors.New("'recovery -> PartitionType' only accept \"gpt\" or \"mbr\"")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.Bootloader == "" {
 		err = errors.New("'recovery -> Bootloader' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	} else if config.Configs.Bootloader != "grub" && config.Configs.Bootloader != "u-boot" {
 		err = errors.New("'recovery -> Bootloader' only accept \"grub\" or \"u-boot\"")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.Swap != true && config.Configs.Swap != false {
 		err = errors.New("'configs -> swap' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Configs.SwapSize == "" {
 		err = errors.New("'configs -> swapsize' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Recovery.Type == "" {
 		err = errors.New("'recovery -> type' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	} else if config.Recovery.Type != FACTORY_RESTORE && config.Recovery.Type != HEADLESS_INSTALLER && config.Recovery.Type != FACTORY_INSTALL {
 		err = errors.New(fmt.Sprintf("'recovery -> type' only accept", FACTORY_RESTORE, HEADLESS_INSTALLER, FACTORY_INSTALL))
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Recovery.RecoverySize == "" {
 		err = errors.New("'recovery -> recoverysize' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	if config.Recovery.FsLabel == "" {
 		err = errors.New("'recovery -> filesystem-label' field not presented")
-		logger.Noticef(err.Error())
+		log.Printf(err.Error())
 	}
 
 	return err
